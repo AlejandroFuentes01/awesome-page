7447a449030644621211968de69f8a8a0277ac78
JuanPabloVerduzcoCampos <verduzcojuanpablo0@gmail.com>
2026-01-13 11:00:43 -0600

    feat: update product slug for INNEDIT - GOLDEN ALLSTAR

--- src/data/products.ts
+++ src/data/products.ts
@@ -64,7 +64,7 @@ export const products: Product[] = [
         pageClass: "muratraviselectric"
     },
     {
-        slug: "goldenallstar",
+        slug: "inneditallstar",
         title: "INNEDIT - GOLDEN ALLSTAR",
         subtitle: "GOLDEN ALLSTAR",
         image: "/cactus-city/GOLDENALLSTAR.webp",
